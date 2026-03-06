587a7310affeedf8363a0d8a097e18229b9d4ef8
yucongyong <155540321+yucongyong@users.noreply.github.com>
2026-03-06 17:20:55 +0800

    Add files via upload

---
+++ youdata-token/scripts/gen_token.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# 有数平台Token生成脚本
+# 用法: ./gen_token.sh <tokenKey> <domain>
+
+TOKEN_KEY=$1
+DOMAIN=$2
+
+if [ -z "$TOKEN_KEY" ] || [ -z "$DOMAIN" ]; then
+    echo "用法: ./gen_token.sh <tokenKey> <domain>"
+    echo "示例: ./gen_token.sh 17727621338145dba6514670cb81872b45751 https://d.youdata.netease.com"
+    exit 1
+fi
+
+curl -s -X POST "${DOMAIN}/api/dash/util/genToken" \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"tokenType\": \"tokenKey\",
+    \"key\": \"${TOKEN_KEY}\",
+    \"expiryTime\": 600,
+    \"once\": true
+  }"
